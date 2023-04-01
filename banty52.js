2beaf6428e11070f69a1ace4e3c1bdcc92a9c6a6
krunalbhongade <126875304+krunalbhongade@users.noreply.github.com>
2023-04-01 18:44:10 +0530

    functions, greet text, return valve

---
+++ banty52.js
@@ -0,0 +1,35 @@
+console.log("This is tutorial 52");
+
+function greet (name1,greetText="Greeting from Avin Front-end"){
+    console.log(greetText + "" + name1)
+    console.log(name1 + "is hot")
+}
+
+function sum (a,b,c){
+    let d = a + b + c;
+    return d;
+}
+
+// let name = "adi";
+let name1 = "Goti" ;
+let name2 = "Aditya" ;
+let name3 = "Krunal" ;
+greetText = "Welcome";
+
+greet (name1,greetText);
+greet (name2,greetText);
+greet (name3);
+
+let returnVal = sum(1,2,3);
+console.log(returnVal);
+
+// let returnVal = greet(name3);
+// console.log(returnVal);
+
+// greet (name3);
+
+
+
+console.log(name1 + "Brilliant guy");
+console.log(name2 + "Brilliant guy");
+console.log(name3 + "Brilliant guy");
